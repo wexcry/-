194086232987e496999d1fa9d1d2bc3eaee8b95a
wexcry <vivivivivi714@gmail.com>
2025-05-27 14:59:02 +0300

    Add files via upload

---
+++ 1 задание.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _74
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада: ");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Введите срок вклада в месяцах: ");
+            int period = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= period; i++)
+            {
+                sum += sum * 0.07M;
+            }
+            Console.WriteLine($"После {period} месяцев сумма вклада составит {sum}");
+            Console.ReadKey();
+        }
+    }
+}
