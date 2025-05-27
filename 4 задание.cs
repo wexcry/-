6bd25607d4d221a140615387218a709952295c1e
wexcry <vivivivivi714@gmail.com>
2025-05-27 15:02:47 +0300

    Add files via upload

---
+++ 4 задание.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_фвфыв
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите первое число: ");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введите второе число: ");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+
+                if (number1 >= 0 && number1 <= 10 && number2 >= 0 && number2 <= 10)
+                {
+                    Console.WriteLine($"Результат умножения: {number1 * number2}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Недопустимые числа! Пожалуйста, введите числа от 0 до 10.");
+                }
+            }
+        }
+    }
+}
